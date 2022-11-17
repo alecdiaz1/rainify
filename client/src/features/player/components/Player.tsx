f2067f42d51df257159bdcf5805a5eea79798228
Alec Diaz <alecdiaz1@yahoo.com>
2022-11-16 21:19:09 -0800

    feat: add ability to play/pause songs

--- client/src/features/player/components/Player.tsx
+++ client/src/features/player/components/Player.tsx
@@ -10,6 +10,7 @@ import { useAppDispatch } from 'hooks/useAppDispatch';
 import { RootState } from 'stores/store';
 import { SONGS } from 'songs';
 import { formatArtists } from 'utils/formatArtists';
+import { useEffect, useState } from 'react';
 
 export const Player = () => {
   const isPlaying = useAppSelector((state: RootState) => state.player.playing);
@@ -17,13 +18,26 @@ export const Player = () => {
     (state: RootState) => state.player.queue[0],
   );
   const currentSongInfo = SONGS.find((song) => song.id === currentSongId);
+  const [audio, setAudio] = useState(new Audio(currentSongInfo?.fileUrl));
 
   const dispatch = useAppDispatch();
 
+  useEffect(() => {
+    audio.pause();
+    setAudio(new Audio(currentSongInfo?.fileUrl));
+  }, [currentSongInfo]);
+
+  useEffect(() => {
+    audio.play().then();
+    dispatch(play());
+  }, [audio]);
+
   const onPlayPressed = () => {
     if (isPlaying) {
+      audio.pause();
       dispatch(pause());
     } else {
+      audio.play().then();
       dispatch(play());
     }
   };
--- client/src/songs.ts
+++ client/src/songs.ts
@@ -3,34 +3,29 @@ import { Song } from 'features/songs';
 export const SONGS: Song[] = [
   {
     id: 1,
-    title: 'better without u',
+    title: 'long distance',
     artist: ['niko rain'],
-    fileUrl: 'public/bwu.mp3',
+    fileUrl:
+      'https://nopixel-rainify.s3.amazonaws.com/songs/niko+rain+-+long+distance.mp3',
     coverUrl:
       'https://image.petmd.com/files/styles/863x625/public/2022-06/rat.blanket.jpg',
   },
   {
     id: 2,
-    title: 'goodbye',
+    title: 'never be the same! (2007 ver.)',
     artist: ['niko rain'],
-    fileUrl: 'public/goodbye.mp3',
+    fileUrl:
+      'https://nopixel-rainify.s3.amazonaws.com/songs/niko+rain+-+never+be+the+same!+(2007+ver).mp3',
     coverUrl:
       'https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/25RXAHHFJEI6TMFGHUBXEG4F54.jpg',
   },
   {
     id: 3,
-    title: 'betrayed',
+    title: 'SUBMIT',
     artist: ['niko rain', 'saint noelle'],
-    fileUrl: 'public/bwu.mp3',
+    fileUrl:
+      'https://nopixel-rainify.s3.amazonaws.com/songs/niko+rain+x+saint+noelle+-+SUBMIT.mp3',
     coverUrl:
       'https://image.petmd.com/files/styles/863x625/public/2022-06/rat.blanket.jpg',
-  },
-  {
-    id: 4,
-    title: 'rather be lonely',
-    artist: ['saint noelle'],
-    fileUrl: 'public/goodbye.mp3',
-    coverUrl:
-      'https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/25RXAHHFJEI6TMFGHUBXEG4F54.jpg',
   },
 ];
