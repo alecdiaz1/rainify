e487b816e3e96db0e9b1bf4572b656eace64e1c4
Alec Diaz <alecdiaz1@yahoo.com>
2022-11-28 20:16:24 -0800

    feat: add backwards navigation

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import { RootState } from 'stores/store';
 import { useAppDispatch } from 'hooks/useAppDispatch';
 import { setSongDetailVisible } from 'features/player/playerSlice';
 import { VolumeSlider } from 'features/player/components/VolumeSlider';
-import { Link, useMatch } from 'react-router-dom';
+import { Link, useMatch, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
   const isSongDetailVisible = useAppSelector(
@@ -17,6 +17,7 @@ export const Navbar = () => {
 
   const dispatch = useAppDispatch();
   const isHomePage = useMatch('/');
+  const navigate = useNavigate();
 
   return (
     <div className="grid grid-cols-3 px-4 mb-4 items-center sticky top-0 bg-neutral-800 drop-shadow w-full">
@@ -28,9 +29,9 @@ export const Navbar = () => {
         />
       )}
       {!isHomePage && !isSongDetailVisible && (
-        <Link to="/" className="w-2">
+        <button onClick={() => navigate(-1)} className="w-2">
           <RiArrowDropLeftLine size={48} className="-ml-4 fill-white" />
-        </Link>
+        </button>
       )}
       <Link to="/" className="my-4 col-start-2 col-end-2">
         <h2 className="text-3xl text-center text-white font-bold">rainify</h2>
